f862a72973b15e022f2fe82ca3531741fad894f0
rmnavarro <rafaelnavarro@gmail.com>
2021-03-01 20:34:32 -0600

    Routes created for projects

--- routes_bflc.js
+++ routes_bflc.js
@@ -4,23 +4,29 @@ const {
     bfloc_location,
     bfloc_business_function } = require("./db_model.js")
 
+var myBF;
+var myLOC;
+var errorBFL;
 
 exports.list = function(req, res){
 	project.findById(req.params.id_pro, (err, foundProject)=> {
-		if(err){alert("ERROR FINDING PROJECT..."); res.redirect("/projects");}
-		else { 	
+		if(err){
+            console.log("ERROR FINDING PROJECT...");
+            res.redirect("/projects");
+        } else { 	
 			bfloc_location.find({}, (err, locations) => {
-				if(err){ console.log("DataBase Error!!")}
-				else { 
+				if(err){ 
+                    console.log("ERROR FINDING LOCATIONS...")
+                } else { 
                     myLOC =  locations.copyWithin();
                     bfloc_business_function.find({}, (err, business_functions) => {
-                        if(err){ console.log("DataBase Error!!")}
-                        else { 
+                        if(err){ 
+                            console.log("DataBase Error!!")
+                        } else { 
                             myBF =  business_functions.copyWithin();
                             res.render("editBfloc", {project:foundProject, business_functions:business_functions, locations: locations, edit: false, errors:errorBFL });}
                         })
-                    
-                     }
+                }
 				})
 			}					
 	});
